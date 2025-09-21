00243a60249c094cb52373ce3253b0013bc667e7
abilash-mosuru <134186811+abilash-mosuru@users.noreply.github.com>
2025-09-21 18:11:44 +0530

    Update app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AuthComponent  {
-    // title = 'frontend';
-  constructor() {}
-
-  
+export class AppComponent {
+  title = 'frontend';
 }
