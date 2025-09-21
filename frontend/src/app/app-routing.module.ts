d1dfef44851545323278dfd5650178703be5c4b5
abilash-mosuru <134186811+abilash-mosuru@users.noreply.github.com>
2025-09-21 18:10:39 +0530

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent } from "./components/login/login.component";
-import { UserComponent } from "./components/user/user.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", component: LoginComponent },
-  { path: "login", component: LoginComponent },
-  { path: "add-user", component: UserComponent },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'supplylink',
+    loadChildren: () => import('./supplylink/supplylink.module').then((m) => m.SupplyLinkModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/auth',  // Redirect to 'auth' route by default
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AppRoutingModule {}
